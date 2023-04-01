3af5aa0f722ead7c44b785fb081a1fca28178339
AzureAD\MarkoManic <kursqa@gmail.com>
2023-04-01 12:37:39 +0200

    Initial Commit

---
+++ AutomationExerciseFW/Helpers/Base.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace AutomationExerciseFW.Helpers
+{
+    [Binding]
+    public class Base
+    {
+        public static IWebDriver Driver { get; set; }
+
+        [Before]
+        public static void BeforeScenario()
+        {
+            Driver = new ChromeDriver();
+            Driver.Manage().Window.Maximize();
+            var url = "https://automationexercise.com/";
+            Driver.Url = url;
+        }
+
+        [AfterScenario]
+        public static void AfterScenario()
+        {
+            Driver.Quit();
+        }
+    }
+}
---
+++ AutomationExerciseFW/Helpers/TestConstants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationExerciseFW.Helpers
+{
+    public static class TestConstants
+    {
+        //existing user data
+        public const string Username = "kursqa+9@gmail.com";
+        public const string Password = "Gorandrulic999";
+
+        //new user data
+        public const string FirstName = "Marko";
+        public const string LastName = "Manic";
+        public const string Address = "Bulevar Milutina Milankovic 7";
+        public const string Country = "Canada";
+        public const string State = "Ontario";
+        public const string City = "Montreal";
+        public const string ZipCode = "10010";
+        public const string Phone = "0800100100";
+    }
+}
